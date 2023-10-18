1b676460632707c6833713c3c17821aa1f573496
ermi1999 <ermiyasabiye1999@gmail.com>
2023-10-18 07:58:50 -0700

    task 2

--- pint.c
+++ pint.c
@@ -11,6 +11,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
